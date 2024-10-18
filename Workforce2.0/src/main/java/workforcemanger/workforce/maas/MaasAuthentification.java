b63047bed18ad68d088c06874c99bccc93e312d6
mouadHILLALI <mauadtoto001@gmail.com>
2024-10-18 10:23:00 +0100

    adding all files and creating the project

--- Workforce2.0/src/main/java/workforcemanger/workforce/maas/MaasAuthentification.java
+++
@@ -1,7 +0,0 @@
-package workforcemanger.workforce.maas;
-
-public interface MaasAuthentification {
-    abstract <T> Object login (T t);
-    abstract <T> void logout (T t);
-    abstract <T> Object register (T t);
-}
--- Workforce2.0/src/main/java/workforcemanger/workforce/repository/JobOffer/JobOfferRepository.java
+++
@@ -1,6 +0,0 @@
-package workforcemanger.workforce.repository.JobOffer;
-
-import workforcemanger.workforce.repository.Repository;
-
-public interface JobOfferRepository extends Repository {
-}
